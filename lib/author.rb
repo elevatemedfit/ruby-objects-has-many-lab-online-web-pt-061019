682023e6d71873a2a4cf4532f06cb18e987470bc
Chris Junker <elevatemedfit@gmail.com>
2019-07-16 01:19:55 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -2,30 +2,30 @@ class Author
 
   attr_accessor :name ,:posts
 
-  @@post_count= 0
+
 
   def initialize(name)
     @name = name
     @posts = []
-    #post_count +=1
+
   end
 
   def add_post(post)
    @posts << post
     post.author = self
-    @@post_count +=1
+
 end
 
   def add_post_by_title(name)
     post = Post.new(name)
     @posts << post
     post.author = self
-    @@post_count +=1
+
   end
 
 
-   def post_count(post_count)
-     @@post_count
+   def post_count
+     @posts.all
    end
 
   def posts
