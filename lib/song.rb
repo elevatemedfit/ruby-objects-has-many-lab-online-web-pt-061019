140619e913bfb338ee949d9707cacc08b0099023
Chris Junker <elevatemedfit@gmail.com>
2019-07-16 00:51:09 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,25 @@
+class Song
+  attr_accessor :artist, :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def save
+    self.class.all << self
+  end
+
+
+
+
+
+
+
+end
